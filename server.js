374cbcde608705bad146c92e11c4a9649523f5a3
Victor Hoang <vic.hoang@yahoo.com>
2015-11-08 17:06:21 -0800

    Add cron job

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var fs = require('fs');
 var express = require('express');
 var app = express();
+var CronJob = require('cron').CronJob;
 
 app.set('port', (process.env.PORT || 5000));
 
@@ -11,3 +12,24 @@ app.get('/', function(req, res) {
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
+
+var job = new CronJob({
+  cronTime: '*/5 * * * * *', // everyday at 10 second intervals
+  onTick: function() {
+    var log = 'count.txt';
+
+    fs.readFile(log, 'utf-8', function(err, count) {
+      if (err) return console.log(err);
+      count = count ? parseInt(count) + 1 : 0;
+
+      fs.writeFile(log, count, function (err) {
+        if (err) return console.log(err);
+        console.log(count);
+      });
+    });
+  },
+  start: true,
+  timeZone: 'America/Los_Angeles'
+});
+
+job.start();
